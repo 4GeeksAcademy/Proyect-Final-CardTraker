62c8393275ba650e1a40d072e53e90fd59a34c5c
afbeltranv <124716013+afbeltranv@users.noreply.github.com>
2023-07-03 19:59:03 +0000

    Relacion entre dbs de User y Cards lograda

--- src/api/models.py
+++ src/api/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     user_name = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_admin = db.Column(db.Boolean(), unique=False, nullable=False)
+    cards = db.relationship('Cards', backref='user', lazy=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -24,8 +25,8 @@ class Cards(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     card_provider = db.Column(db.String(120), nullable=False)
     last_four = db.Column(db.Integer(), nullable=False)
-    bank_name = db.Column(db.String(), nullable=False)
-    user_id = db.Column(db.Integer(), nullable=False)
+    bank_name = db.Column(db.String(), nullable=False)    
+    card_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f'<Cards {self.id}>'
