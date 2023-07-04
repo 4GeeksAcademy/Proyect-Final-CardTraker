841b507e3efaed73f7a1d9ce13b4bc9dc57e36d8
Vanessa <126210620+nessarz93@users.noreply.github.com>
2023-07-04 03:00:17 +0000

    Inicio de Home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,21 +3,25 @@ import { Link } from "react-router-dom"
 
 export const Navbar = () => {
     return(
-        <nav className="navbar" style="background-color: #e3f2fd;">
-            <div className="container-fluid">
-                <a className="navbar-brand" to="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <Link className="nav-link active" aria-current="page" to="#">About</Link>
-                    <Link className="nav-link" to="#">Contact</Link>
-                    <Link className="nav-link" to="#">Opportunities</Link>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="btn-group" role="group" aria-label="Basic outlined example">
+                    <button type="button" class="btn btn-outline-primary">About</button>
+                    <button type="button" class="btn btn-outline-primary">Contact</button>
+                    <button type="button" class="btn btn-outline-primary">Opportunities</button>
                 </div>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <button type="button" class="btn btn-outline-primary me-md-2">Register</button>
+                    <button type="button" class="btn btn-outline-primary">Login</button>
                 </div>
-            </div>
-        </nav>
+          </div>
+        </div>
+      </nav>
     )
 };
 
