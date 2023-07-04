2b61e56432e678685a8385b9a26c87e262601c76
Vanessa <126210620+nessarz93@users.noreply.github.com>
2023-07-04 01:39:47 +0000

    Continuación Navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,9 +3,21 @@ import { Link } from "react-router-dom"
 
 export const Navbar = () => {
     return(
-        <div>
-            
-        </div>
+        <nav className="navbar" style="background-color: #e3f2fd;">
+            <div className="container-fluid">
+                <a className="navbar-brand" to="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <Link className="nav-link active" aria-current="page" to="#">About</Link>
+                    <Link className="nav-link" to="#">Contact</Link>
+                    <Link className="nav-link" to="#">Opportunities</Link>
+                </div>
+                </div>
+            </div>
+        </nav>
     )
 };
 
