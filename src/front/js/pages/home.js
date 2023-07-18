1d572480957199dc889d72737740e4576ec4eddc
afbeltranv <124716013+afbeltranv@users.noreply.github.com>
2023-07-18 06:07:03 +0000

    modificación estable

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -21,7 +21,7 @@ export const Home = () => {
 	return (
 		<>		
 			<div className="text-center mt-5">
-				<h1>Hello Rigo!!</h1>
+				<h1>Hello Rigo!!!!!</h1>
 				<p>
 					<img src={rigoImageUrl} />
 				</p>
